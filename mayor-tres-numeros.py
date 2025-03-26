f02b65c4a2297a79652fff90b3586c305242ca44
71850190-luis <71850190@continental.edu.pe>
2025-03-26 11:35:09 -0500

    Segundo commit de mayor  de tres numeros usando funciones y reutilizacion de codigo.

--- mayor-tres-numeros.py
+++ mayor-tres-numeros.py
@@ -1,23 +1,23 @@
 #EJERCICIO: DETERMINA EL MAYOR DE 3 NÚMEROS INGRESADOS POR EL TECLADO
 
+def intercambio1(numero1,numero2):
+    temporal = numero1
+    numero1 = numero2
+    numero2 = temporal
+    return(numero1,numero2)
+
 numero1 = int(input("Ingresa el primer numero: "))
 numero2 = int(input("Ingresa el segundo numero: "))
 numero3 = int(input("Ingresa el tercer numero: "))
 
 if numero1 > numero2:
-    temporal = numero1
-    numero1 = numero2
-    numero2 = temporal
+    numero1,numero2 = intercambio1(numero1,numero2)
 
 if numero2 > numero3:
-    temporal = numero2
-    numero2 = numero3
-    numero3 = temporal
+    numero2,numero3 = intercambio1(numero2,numero3)
     
 if numero1 > numero2:
-    temporal = numero1
-    numero1 = numero2
-    numero2 = temporal
+     numero1,numero2 = intercambio1(numero1,numero2)
 
 print(f"Numeros ordenados {numero1,numero2,numero3}")
 print(f"El mayor es: {numero3}")
