34b9fa5f0a41ccede7b9fc02e5b550567222ddb4
71850190-Luis <71850190@continental.edu.pe>
2025-04-09 11:24:17 -0500

    Commit para probar las ramas

---
+++ circulo.py
@@ -0,0 +1,11 @@
+class Circulo:
+    PI = 3.1416
+    def __init__(self,radio):
+        self.radio = radio
+
+def circunferencia(self):
+    return 2*self.PI*self.radio 
+
+if __name__ == "__main__":
+    instancia_circulo = Circulo()
+    print(f"La circunferencia es: {instancia_circulo.circunferencia}")
